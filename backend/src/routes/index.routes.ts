af94d5b64fbf75091ae8de3657aa60500dab9a8e
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 12:39:00 +0100

    fix(auth): version api endpoint

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,13 +2,13 @@ import {Router} from "express"
 import dotenv from 'dotenv'
 import webhookRoutes from "./webhook.routes.js"
 import { verifySignature } from "../middleware/webhook.middleware.js"
-import vendorRoutes from "./auth.routes.js"
+import authRoutes from "./auth.routes.js"
 const router:Router =Router()
 
 dotenv.config()
 
 
 router.use("/webhook",verifySignature, webhookRoutes)
-router.use("/vendor", vendorRoutes)
+router.use("/v1/auth", authRoutes)
 
 export default router;
