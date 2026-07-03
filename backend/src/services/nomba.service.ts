5f152cbec0a2d28d5319ffa43860af2622d78c87
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 08:59:17 +0100

    Merge 'feature/vendor' into 'main'

---
+++ backend/src/services/nomba.service.ts
@@ -0,0 +1,7 @@
+/**
+ * https://api.nomba.com/v1/transfers/banks --fetch bank codes
+ * https://api.nomba.com/v2/transfers/bank/{subAccountId} ---account transfer from sub acct
+ * https://api.nomba.com/v1/transfers/bank/lookup ---bank account lookup 
+ * https://api.nomba.com/v1/transactions/requery/{sessionId} --Transaction status requery
+ * 
+ */
