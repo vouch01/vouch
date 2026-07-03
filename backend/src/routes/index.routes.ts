277f061e6c20a3a00359faeb8823ee34f657e9be
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 01:36:26 +0100

    feat(vendor): implement vendor account creation endpoint

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,13 +2,13 @@ import {Router} from "express"
 import dotenv from 'dotenv'
 import webhookRoutes from "./webhook.routes.js"
 import { verifySignature } from "../middleware/webhook.middleware.js"
-
+import vendorRoutes from "../routes/vendor.routes.js"
 const router:Router =Router()
 
 dotenv.config()
 
 
 router.use("/webhook",verifySignature, webhookRoutes)
-
+router.use("/vendor", vendorRoutes)
 
 export default router;
---
+++ backend/src/services/vendor.service.ts
@@ -0,0 +1,62 @@
+import { accessToken } from "./token.service.js";
+import bcrypt from "bcrypt";
+import { eq } from "drizzle-orm";
+import { db } from "../db/index.js";
+import { vendors, orders, webhook_events } from "../db/schema.js";
+
+interface VendorInputs {
+  business_name: string;
+  email: string;
+  password: string;
+  bank_code: string;
+  bank_account_number: string;
+  bank_account_name: string;
+}
+
+
+export const createVendor = async (vendorData: VendorInputs) => {
+  try {
+    const {
+      business_name,
+      email,
+      password,
+      bank_code,
+      bank_account_number,
+      bank_account_name,
+    } = vendorData;
+    const existingVendor = await db
+      .select()
+      .from(vendors)
+      .where(eq(vendors.email, email));
+    if (existingVendor.length > 0) {
+      return { status: 409, success: false, message: "user already Exists" };
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newVendor = await db
+      .insert(vendors)
+      .values({
+        business_name,
+        email,
+        password_hash: passwordHash,
+        bank_code,
+        bank_account_number,
+        bank_account_name,
+      })
+      .returning();
+
+      const token = accessToken(newVendor[0]!.id)
+      const {password_hash,  ...vendor} = newVendor[0]!
+
+    return {
+      status: 201,
+      success: true,
+      message: ":vendor account created successfully",
+      token,
+      data:vendor,
+    };
+  } catch (err: any) {
+    console.error("Error occurred while creating vendor:", err.message);
+    return { status: 500, success: false, message: err.message };
+  }
+};
