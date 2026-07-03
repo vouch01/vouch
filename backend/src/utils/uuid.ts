e2397f702ffad3c84d90e7f9a0f8cc8c2bac1478
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 16:46:12 +0100

    feat(auth): add unique otp generator

---
+++ backend/src/utils/uuid.ts
@@ -0,0 +1,14 @@
+import crypto from "crypto";
+
+export function generateUniqueOtp(length:number): string {
+  const digits =
+    "0123456789";
+  let uniqueOtp = "";
+
+  const array = new Uint32Array(length);
+  crypto.getRandomValues(array);
+  for (let i = 0; i < length; i++) {
+    uniqueOtp += digits[array[i]! % digits.length];
+  }
+  return uniqueOtp;  
+}
