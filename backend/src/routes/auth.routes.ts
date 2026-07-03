e251c5aba3f1ca2932a2809a329aaf87df82efd3
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 11:45:51 +0100

    chore(auth): implement login route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router: Router = Router();
 
-import {CreateVendorController} from "../controller/auth.controller.js"
+import {CreateVendorController, LoginVendorController} from "../controller/auth.controller.js"
 // import {authenticate} from "../middleware/auth.middleware.js"
 
 router.post("/signup", CreateVendorController)
+router.post("/login", LoginVendorController)
 
 export default router ;
