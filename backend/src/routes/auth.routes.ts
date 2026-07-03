bfb4cad87edd1f711c9e8f467ba642c129088dca
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-03 11:40:57 +0100

    fix: remove unncessary files and import properly

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router: Router = Router();
 
 import {CreateVendorController} from "../controller/auth.controller.js"
-import {authenticate} from "../middleware/auth.middleware.ts.js"
+// import {authenticate} from "../middleware/auth.middleware.js"
 
 router.post("/signup", CreateVendorController)
 
--- backend/src/routes/vendor.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router: Router = Router();
-
-import {CreateVendorController} from "../controller/vendor.controller.js"
-import {authenticate} from "../middleware/auth.middleware.ts.js"
-
-router.post("/signup", CreateVendorController)
-
-export default router ;
