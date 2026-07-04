cb4e3848a047ef2f26cd30621174877158df0e3e
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-04 15:47:34 +0100

    fix: parse cors options properly

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,13 +9,15 @@ const app: express.Application =express()
 
 const allowedOrigins= process.env.ALLOWED_ORIGINS?.split(',')  || []
 
-app.use(cors ({
+const corsOptions:any = {
   origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-}))
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
 
-app.options('*', cors());
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions));
 
 
 app.use(express.json())
