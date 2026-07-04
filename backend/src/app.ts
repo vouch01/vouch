708a1734038ee81372923ee051aebf50a926043f
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-04 15:33:53 +0100

    feat:configure cors for secure resource sharing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from "./routes/index.routes.js"
+import cors from "cors"
 
 dotenv.config()
 
 const app: express.Application =express()
 
+const allowedOrigins= process.env.ALLOWED_ORIGINS?.split(',')  || []
+
+app.use(cors ({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+}))
+
+app.options('*', cors());
+
 
 app.use(express.json())
 app.use(routes)
+
 
 
 app.get("/",function(req:any, res:any){
