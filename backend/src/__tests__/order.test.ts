118f94a6f23c69284597f74cc4dba097f624a670
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-07 20:09:04 +0100

    test(order): add order endpoints test

--- backend/src/__tests__/order.test.ts
+++ backend/src/__tests__/order.test.ts
@@ -8,6 +8,7 @@ jest.setTimeout(15000);
 
 describe("Order Flow", () => {
     let token:string
+    let id:string
 
 beforeAll(async () => {
     const loginRes = await request(app)
@@ -24,16 +25,46 @@ beforeAll(async () => {
     .post("/v1/order/create")
      .set("Authorization", `Bearer ${token}`)
      .send({
-        buyer_phone:"09117053519",
+        buyer_phone:"09192053519",
         item_description:"ows gadgets",
         additional_notes: "Keep it neat",
         delivery_address: "new wplace way ",
          amount: 30000
      })   
+     id =res.body.data.id
 
      expect(res.statusCode).toEqual(201);
     expect(res.body.message).toContain("Successfully");
     expect(res.body.data.item_description).toContain("ows");
     expect(res.body.escrowLink).toBeDefined()
+  })
+  
+   it("returns status code 200 if order is retrieved successfully", async() =>{
+    const res = await request(app)
+    .get(`/v1/order/${id}`)
+     .set("Authorization", `Bearer ${token}`)  
+
+     expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toContain("successfully");
+    expect(res.body.data.item_description).toContain("ows");
+    expect(res.body.escrowLink).toBeDefined()
+  })
+   it.skip("returns status code 200 if all order is retrieved successfully", async() =>{
+    const res = await request(app)
+    .get(`/v1/order/all`)
+     .set("Authorization", `Bearer ${token}`)
+
+     expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toContain("successfully");
+
+  })
+
+  it("returns status code 200 if order is deleted successfully", async() =>{
+    const res = await request(app)
+    .delete(`/v1/order/cancel/${id}`)
+     .set("Authorization", `Bearer ${token}`)
+
+     expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toContain("successfully");
   })
 })
