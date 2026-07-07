0ec5d908bd119ba73b5d49462f85752a1d9ac7e7
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-07 20:59:29 +0100

    chore: confirm processed webhook

--- backend/src/routes/webhook.routes.ts
+++ backend/src/routes/webhook.routes.ts
@@ -1,11 +1,25 @@
 import {Router} from "express"
+import { verifySignature } from "../middleware/webhook.middleware.js";
+import { vendors, orders, webhook_events, otp_tokens } from "../db/schema.js";
+import { eq  } from "drizzle-orm";
+import { db } from "../db/index.js";
+
 const router:Router =Router()
 
-router.post("/nomba",function(req:any, res:any){
+router.post("/nomba", verifySignature, async (req:any, res:any) => {
   const payload = req.body
 
+  const alreadyProcessed = await db.query.webhook_events.findFirst({
+    where: eq(webhook_events.nomba_transaction_id, payload.data.transaction.transactionId)
+  })
+
+  if (alreadyProcessed) {
+    return res.status(200).json({ received: true, duplicate: true })
+  }
+  
   console.log('Webhook Received:', JSON.stringify(payload , null , 2));
   res.status(200).json({received: true})
+
 })
 
 export default router;
