59fabe52120dee5321aa817e239bfe031f8a50fd
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-07 15:00:21 +0100

    feat(nomba): Implement nomba token authorization

---
+++ backend/src/utils/nomba.ts
@@ -0,0 +1,78 @@
+import axios from 'axios'
+
+let cachedToken:string | null = null
+let cachedRefreshToken :string | null = null 
+let tokenExpiryTime: number | null = null
+
+async function issueNewToken(): Promise<void> {
+     const response = await axios.post('https://api.nomba.com/v1/auth/token/issue', {
+    grant_type: 'client_credentials',
+    client_id: process.env.NOMBA_CLIENT_ID,
+    client_secret: process.env.NOMBA_CLIENT_SECRET
+  }, {
+    headers: {
+        'Content-Type': 'application/json',
+         accountId: process.env.NOMBA_ACCOUNT_ID! ,
+        }
+  })
+
+  const { code, data } = response.data
+  if (code !== '00') {
+    throw new Error('Nomba authentication failed')
+  }
+
+    console.log('Nomba Atoken response:', data)
+
+  const { access_token, refresh_token, expiresAt } = data
+  cachedToken = access_token
+  cachedRefreshToken = refresh_token
+  tokenExpiryTime = new Date (expiresAt).getTime()
+}
+
+async function refreshToken():Promise<void>{
+    const response = await axios.post('https://api.nomba.com/v1/auth/token/refresh', {
+        grant_type: "refresh_token",
+        refresh_token: cachedRefreshToken
+    },{
+        headers: {
+            Authorization: `Bearer ${cachedToken}`,
+            'Content-Type': 'application/json',
+            accountId: process.env.NOMBA_ACCOUNT_ID!,
+        }
+    })
+
+    const { code, data } = response.data
+  if (code !== '00') {
+    throw new Error('Nomba authentication failed')
+  }
+
+    console.log('Nomba Rtoken response:', data)
+    const {access_token, refresh_token, expiresAt} = data
+    
+    cachedToken= access_token
+    cachedRefreshToken= refresh_token
+    tokenExpiryTime = new Date (expiresAt).getTime()
+
+}
+
+
+export async function getValidAccessToken():Promise<string>  {
+let currentTime = Date.now()
+let bufferTime= 60000
+
+if(cachedToken && tokenExpiryTime && (currentTime + bufferTime < tokenExpiryTime)){ 
+return cachedToken;
+}
+
+try{
+    if(!cachedToken){
+        await issueNewToken()
+    }else{
+        await refreshToken()
+    }
+    return cachedToken!
+}catch(err:any){
+     console.error("Critical Failure : Could not authenticate with nomba API", err.message)
+     throw new Error ("nomba authentication failed")
+}
+}
