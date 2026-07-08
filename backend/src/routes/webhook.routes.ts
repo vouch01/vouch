7f8f2c8be6899f226d44097b87e2032ea0e67485
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-08 18:11:11 +0100

    chore(webhook): pass payment processing to queue

--- backend/src/routes/webhook.routes.ts
+++ backend/src/routes/webhook.routes.ts
@@ -3,20 +3,14 @@ import { verifySignature } from "../middleware/webhook.middleware.js";
 import { vendors, orders, webhook_events, otp_tokens } from "../db/schema.js";
 import { eq  } from "drizzle-orm";
 import { db } from "../db/index.js";
-
+import {paymentQueue} from "../lib/payment.queue.js"
 const router:Router =Router()
 
 router.post("/nomba", verifySignature, async (req:any, res:any) => {
   const payload = req.body
-
-  const alreadyProcessed = await db.query.webhook_events.findFirst({
-    where: eq(webhook_events.nomba_transaction_id, payload.data.transaction.transactionId)
-  })
-
-  if (alreadyProcessed) {
-    return res.status(200).json({ received: true, duplicate: true })
-  }
   
+  await paymentQueue.add('process-payment', payload)
+
   console.log('Webhook Received:', JSON.stringify(payload , null , 2));
   res.status(200).json({received: true})
 
