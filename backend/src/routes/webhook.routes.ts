ca5090a06f8811e0c7eed0a5be15b20f18f35dde
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-13 18:43:05 +0100

    refactor: support multiple queues with identical payloads by using event_type as unique identifier

--- backend/src/routes/webhook.routes.ts
+++ backend/src/routes/webhook.routes.ts
@@ -1,20 +1,28 @@
-import {Router} from "express"
+import { Router } from "express";
 import { verifySignature } from "../middleware/webhook.middleware.js";
 import { vendors, orders, webhook_events, otp_tokens } from "../db/schema.js";
-import { eq  } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { db } from "../db/index.js";
-import {paymentQueue} from "../lib/payment.queue.js"
-const router:Router =Router()
+import { paymentQueue } from "../lib/payment.queue.js";
+const router: Router = Router();
 
-router.post("/nomba", verifySignature, async (req:any, res:any) => {
-  const payload = req.body
-  
-  await paymentQueue.add('process-payment', payload)
-  console.log('payement-processing job queued')
+router.post("/nomba", verifySignature, async (req: any, res: any) => {
+  const payload = req.body;
 
-  console.log('Webhook Received:', JSON.stringify(payload , null , 2));
-  res.status(200).json({received: true})
+  switch (payload.event_type) {
+    case "payout_success":
+      await paymentQueue.add("process-payment", payload);
+      console.log("payment-processing job queued");
+      break;
+    case "payment_success":
+      await paymentQueue.add("payout_confirmation", payload);
+      break;
+    default:
+      console.warn("Unhandled webhook event type:", payload.event_type);
+  }
 
-})
+  console.log("Webhook Received:", JSON.stringify(payload, null, 2));
+  res.status(200).json({ received: true });
+});
 
 export default router;
