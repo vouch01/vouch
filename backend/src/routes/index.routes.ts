66182895e3780a0c19236fa3f2725a7a943fee50
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-13 18:02:40 +0100

    fix: remove duplicate signature verification

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,7 +1,6 @@
 import {Router} from "express"
 import dotenv from 'dotenv'
 import webhookRoutes from "./webhook.routes.js"
-import { verifySignature } from "../middleware/webhook.middleware.js"
 import authRoutes from "./auth.routes.js"
 import profileRoutes from './profile.routes.js'
 import orderRoutes from "./order.routes.js"
@@ -13,7 +12,7 @@ const router:Router =Router()
 dotenv.config()
 
 
-router.use("/webhook",verifySignature, webhookRoutes)
+router.use("/webhook", webhookRoutes)
 router.use("/v1/auth", authRoutes)
 router.use("/v1/profile", profileRoutes)
 router.use("/v1/order", orderRoutes)
